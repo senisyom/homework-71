d0e210c2ebdbcbffe9480caf9a4d67ffebe22b0a
Begmatov Almaz <lolkek13@inbox.ru>
2024-11-21 13:48:35 +0600

    App init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <div></div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+      <div className="container-sm">
+        <NavLink to="/">
+          <span className="navbar-brand">Turtle Pizza Admin</span>
+        </NavLink>
+
+        <ul className="navbar-nav mr-auto flex-row gap-2 flex-nowrap">
+          <li className="nav-item">
+            <NavLink to="/admin/dishes" className="nav-link">
+              Dishes
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/admin/orders" className="nav-link">
+              Orders
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
